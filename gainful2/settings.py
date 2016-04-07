dee1ddf111b809d8e7e837aaa3c1a0138c5f79b2
rgscherf <rgscherf@gmail.com>
2016-04-07 12:43:36 -0400

    static collection

--- gainful2/settings.py
+++ gainful2/settings.py
@@ -134,6 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, '../parsing/static'),)
+STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'), )
