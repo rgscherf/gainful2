da8dec7ec48b121a166b1b31cc6e4db19a3d332e
rgscherf <rgscherf@gmail.com>
2016-04-10 21:41:31 -0400

    style updates

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
 source bin/activate
 cd elm
 elm-make src/Main.elm --output="../parsing/static/parsing/elm.js"
+cd ..
+sass parsing/style.scss parsing/static/parsing/style.css
+python3 manage.py collectstatic --noinput
+
