8ef2be2ff15ab8f993e8e0c74028dcf8e1d34369
rgscherf <rgscherf@gmail.com>
2016-04-27 12:21:34 -0400

    settings

--- gainful2/settings.py
+++ gainful2/settings.py
@@ -148,6 +148,5 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, '../parsing/static'),
-                     os.path.join(PROJECT_ROOT, 'static'),                    )
+STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, '../parsing/static'),)
 
