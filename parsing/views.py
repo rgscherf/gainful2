c2a83c05666020ce0885febee87baecaf286d2ff
rgscherf <rgscherf@gmail.com>
2016-05-19 12:24:52 -0400

    fixed maybe

--- parsing/views.py
+++ parsing/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from parsing.models import Job
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
 from parsing.serializers import JobSerializer
 
 
@@ -14,4 +13,38 @@ class JobViewSet(viewsets.ModelViewSet):
     serializer_class = JobSerializer
 
 def index(request):
-    return render(request, 'parsing/index.html', {})
+    jobs = get_jobs()
+    print(type(jobs))
+    return render(request, 'parsing/index.html', {'jobs_json': jobs})
+
+def get_jobs():
+    objs = Job.objects.all().order_by("-date_posted")
+    serializer = []
+    for o in objs:
+        serializer.append(JobSerializer(o).data)
+    content = JSONRenderer().render(serializer)
+    return content
+
+
+# filename = os.path.abspath(".") + "/parsing/static/parsing/jobs.json"
+
+# class Command(BaseCommand):
+#     def handle(self, *args, **kwargs):
+#         self.write_jobs(filename)
+
+#     def write_jobs(self, file):
+#         objs = Job.objects.all().order_by("-date_posted")
+#         print("Found {} jobs in DB".format(len(objs)))
+#         print("Writing jobs to file...")
+#         serializer = []
+#         for o in objs:
+#             serializer.append(JobSerializer(o).data)
+#         content = JSONRenderer().render(serializer)
+#         with open(file, "wb") as FILE:
+#             FILE.write(content)
+#         if os.path.isfile(file):
+#             print("Wrote jobs.json to {}".format(file))
+#         else:
+#             print("Failed to write jobs.json to {}".format(file))
+
+
