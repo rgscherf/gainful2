14cb7c49d5b2e155e92c07ecd1e58dc386b0ddb3
rgscherf <rgscherf@gmail.com>
2016-11-30 18:27:21 -0500

    stop failing on exceptions--skip instead

--- parsing/parsinglib/jobcontainer.py
+++ parsing/parsinglib/jobcontainer.py
@@ -1,7 +1,9 @@
 from ..models import Job
 import datetime
 
+
 class JobContainer():
+
     def __init__(self):
         self.organization = None
         self.title = None
@@ -19,7 +21,8 @@ class JobContainer():
         Remember to use this function before doing any intense parsing operations.
         """
         if not self.url_detail:
-            raise KeyError("Queried record uniqueness before detail URL set: {}".format(self))
+            raise KeyError(
+                "Queried record uniqueness before detail URL set: {}".format(self))
         else:
             if len(Job.objects.filter(url_detail=self.url_detail)) == 0:
                 return True
@@ -30,35 +33,36 @@ class JobContainer():
     def cleanup(self):
         self.title = self.title.title() if self.title.isupper() else self.title
         self.salary_amount = 0 if self.salary_amount == None else self.salary_amount
-        self.salary_waged = True if self.salary_amount < 5000 else False # totally arbitray amount
+        # totally arbitray amount
+        self.salary_waged = True if self.salary_amount < 5000 else False
         self.date_collected = datetime.date.today()
 
     def validate(self):
         field_dict = self.__dict__
-        attributes = {k:v for k, v in field_dict.items() if not k.startswith("_")}
+        attributes = {
+            k: v for k, v in field_dict.items() if not k.startswith("_")}
         for k, v in attributes.items():
             if v == None:
-                raise KeyError("Job {} was missing {}".format(self.url_detail, k))
+                raise KeyError(
+                    "Job {} was missing {}".format(self.url_detail, k))
 
     def save(self):
         """ Save job to DB, after final checks.
         """
-        if not self.is_unique(): # failsafe in case we forgot to check this earlier.
-            print("{} tried to save a job that is not unique!".format(self.organization))
+        if not self.is_unique():  # failsafe in case we forgot to check this earlier.
+            print(
+                "{} tried to save a job hat is not unique!".format(self.organization))
             return
         self.cleanup()
-        self.validate()
+        try:
+            self.validate()
+        except KeyError as err:
+            print("|| EXCEPTION")
+            print("|| ", err)
+            return
+
         print("Saved job to DB: {}".format(self))
-        j = Job(organization=self.organization
-                , title=self.title
-                , division=self.division
-                , date_posted=self.date_posted
-                , date_closing=self.date_closing
-                , url_detail=self.url_detail
-                , salary_waged=self.salary_waged
-                , salary_amount=self.salary_amount
-                , region=self.region
-                , date_collected = self.date_collected
+        j = Job(organization=self.organization, title=self.title, division=self.division, date_posted=self.date_posted, date_closing=self.date_closing, url_detail=self.url_detail, salary_waged=self.salary_waged, salary_amount=self.salary_amount, region=self.region, date_collected=self.date_collected
                 )
         j.save()
 
